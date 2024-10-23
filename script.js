f443f63bd253baac22f52a401db0593da02c33b7
justsanchez <alvarosanchez9261@gmail.com>
2024-10-23 13:44:04 -0400

    adjusted the scroll reveal on the team cards

--- script.js
+++ script.js
@@ -266,4 +266,4 @@ document.addEventListener("click", function (event) {
 // leave last, its prone to mess up some code
 
 //scroll reveal
-ScrollReveal().reveal(".card", { easing: "ease-in" });
+ScrollReveal().reveal(".card", { easing: "ease-in", duration: 20 }); // default was 1000ms
